e42e2e72d9444ef3fc509f0ac2637989c95917ed
golbanv <golbanvladislav@gmail.com>
2020-01-21 20:14:51 +0100

    Add 'edit' functionality

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const app = express();
+const port = 4000;
+
+mongoose.connect("mongodb://localhost/blog_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+let blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now() }
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("Error man");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+app.post("/blogs", function(req, res) {
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      // res.redirect("/blogs");
+      console.log(err);
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
+
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("edit", { blog: foundBlog });
+    }
+  });
+});
+
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+    err,
+    updatedBlog
+  ) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+app.get("*", function(req, res) {
+  res.send("nothing there bro");
+});
+
+app.listen(port, function() {
+  console.log("Listening on port " + port + " BlogApp!");
+});
